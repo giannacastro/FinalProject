b86610e6216a9714706bded392bedffcde684e34
giannacastro <152915333+giannacastro@users.noreply.github.com>
2024-04-24 17:50:13 -0400

    Update TaxonomyNode.java

--- TaxonomyNode.java
+++ TaxonomyNode.java
@@ -3,57 +3,28 @@ import java.util.List;
 
 public class TaxonomyNode {
 
-    private TaxonomyEnum rank; // Use specific enum instead of Rank
+    private Enum rank;
 
     private String name;
 
     private List<TaxonomyNode> children;
 
-    public TaxonomyNode(TaxonomyEnum rank, String name) {
-        this.rank = rank; // Set to appropriate enum
+    public TaxonomyNode(Enum rank, String name) {
+        this.rank = rank;
         this.name = name;
         this.children = new ArrayList<>();
     }
 
-    public TaxonomyEnum getRank() {
-        return rank; // Return specific enum
+    public void addChild(TaxonomyNode child) {
+        this.children.add(child);
     }
 
     public String getName() {
         return name;
     }
 
-    public List<TaxonomyNode> getChildren() {
-        return children;
+    public Enum getRank() {
+        return rank;
     }
 
-    public void addChild(TaxonomyNode child) {
-        children.add(child);
-    }
-
-    public boolean containsChild(String childName) {
-        for (TaxonomyNode child : children) {
-            if (child.getName().equals(childName)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public TaxonomyNode getChild(String childName) {
-        for (TaxonomyNode child : children) {
-            if (child.getName().equals(childName)) {
-                return child;
-            }
-        }
-        return null;
-    }
-
-    public String[] getChildrenNames() {
-        String[] names = new String[children.size()];
-        for (int i = 0; i < children.size(); i++) {
-            names[i] = children.get(i).getName();
-        }
-        return names;
-    }
 }
