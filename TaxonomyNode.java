dffba436a5f5f43344b91a44a50572d90a320c6a
giannacastro <152915333+giannacastro@users.noreply.github.com>
2024-04-24 19:20:11 -0400

    Update TaxonomyNode.java

--- TaxonomyNode.java
+++ TaxonomyNode.java
@@ -1,33 +1,56 @@
-import Domain; // Import Domain
+import java.util.List;
+import java.util.ArrayList;
 
 public class TaxonomyNode {
 
-    private Domain rank;
-
     private String name;
-
     private List<TaxonomyNode> children;
 
-    public TaxonomyNode(Domain rank, String name) {
-        this.rank = rank;
+    public TaxonomyNode(String name) {
         this.name = name;
         this.children = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     public void addChild(TaxonomyNode child) {
         this.children.add(child);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public Domain getRank() {
-        return rank;
-    }
-
     public List<TaxonomyNode> getChildren() {
-        return children;
+        return this.children;
+    }
+
+    public void print() {
+        System.out.println(this.name);
+        for (TaxonomyNode child : this.children) {
+            child.print();
+        }
+    }
+
+    public void printIndent(int indent) {
+        for (int i = 0; i < indent; i++) {
+            System.out.print("-");
+        }
+        System.out.println(this.name);
+        for (TaxonomyNode child : this.children) {
+            child.printIndent(indent + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        TaxonomyNode root = new TaxonomyNode("Root");
+        TaxonomyNode child1 = new TaxonomyNode("Child1");
+        TaxonomyNode child2 = new TaxonomyNode("Child2");
+        root.addChild(child1);
+        root.addChild(child2);
+        child1.addChild(new TaxonomyNode("Grandchild"));
+
+        root.print();
+        root.printIndent(0);
     }
 
 }
+
