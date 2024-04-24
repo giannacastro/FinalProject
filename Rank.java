cf6731bc6b456eead70d39a2a3610575700b3519
giannacastro <152915333+giannacastro@users.noreply.github.com>
2024-04-24 19:12:34 -0400

    Update Rank.java

--- Rank.java
+++ Rank.java
@@ -1,1 +1,30 @@
+public class Rank {
 
+  private String name;
+  private List<Rank> children;
+
+  public Rank(String name) {
+    this.name = name;
+    this.children = new ArrayList<>();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void addChild(String childName) {
+    Rank child = new Rank(childName);
+    children.add(child);
+  }
+
+  public List<Rank> getChildren() {
+    return children;
+  }
+
+  public void print() {
+    System.out.println(name);
+    for (Rank child : children) {
+      child.print();
+    }
+  }
+}
