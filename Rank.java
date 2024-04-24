091d0d26d1589cf7a2d6734b606d731e1c611a55
giannacastro <152915333+giannacastro@users.noreply.github.com>
2024-04-24 19:31:26 -0400

    Update Rank.java

--- Rank.java
+++ Rank.java
@@ -1,7 +1,9 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class Rank extends Domain {
+import Domain;
+
+public class Rank implements Domain {
 
   private String name;
 
@@ -40,4 +42,3 @@ public class Rank extends Domain {
   }
 
 }
-
